7ec411bb462002c6d2194f1041731729fa2682f4
Andrei Nicolae <andreicalutiu@gmail.com>
2026-02-24 00:33:35 +0200

    code cleanup

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require("path");
 
 const app = express();
 
-// Use environment variables in production
 const USERNAME = process.env.APP_USER;
 const PASSWORD = process.env.APP_PASS;
 const SESSION_SECRET = process.env.SESSION_SECRET;
@@ -20,7 +19,6 @@ app.use(
         cookie: {
             httpOnly: true,
             sameSite: "lax",
-            // secure: true, // enable when behind HTTPS
         },
     })
 );
@@ -30,12 +28,10 @@ function requireAuth(req, res, next) {
     return res.redirect("/login");
 }
 
-// Public: login page
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
-// Public: handle login
 app.post("/login", (req, res) => {
     const { username, password } = req.body;
 
@@ -46,23 +42,18 @@ app.post("/login", (req, res) => {
     return res.redirect("/login?err=1");
 });
 
-// Public: logout
 app.post("/logout", (req, res) => {
     req.session.destroy(() => res.redirect("/login"));
 });
 
-// Everything below is protected
 app.use("/", requireAuth);
 
-// Serve protected static files (app.html, network_data.json, etc.)
 app.use(express.static(path.join(__dirname, "public"), {
     setHeaders: (res, filePath) => {
-        // avoid caching json while developing
         if (filePath.endsWith(".json")) res.setHeader("Cache-Control", "no-store");
     }
 }));
 
-// Default route -> app
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "app.html"));
 });
